28cccb6e94373f1a6a5a0ab83159ba3b5d4afc16
Adrian Mora <adriandariom@gmail.com>
2015-06-20 20:54:00 -0400

    Created basic structure and flow

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -1,11 +1,32 @@
 package Core;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Arrays;
+
 /**
  * Created by AdrianM on 6/18/15.
  */
 public class Main {
 
+    public static final String[] VALID_FLAGS = {"--full", "--front", "--config"};
+
     public static void main(String[] args) {
+
+        System.out.println("Made it here");
+
+        String[] urlAndFlags = Utils.parseArgs(args);
+        String url = urlAndFlags[0];
+        String[] flags = Arrays.copyOfRange(urlAndFlags, 1, urlAndFlags.length);
+
+        if (Utils.validateArgs(url, flags)) {
+            WebDriver driver = new ChromeDriver();
+
+        } else {
+            System.out.println(invalidArgMesage);
+        }
+
 
     }
 }
---
+++ src/Core/Utils.java
@@ -0,0 +1,9 @@
+package Core;
+
+/**
+ * Created by AdrianM on 6/20/15.
+ */
+public class Utils {
+
+
+}
