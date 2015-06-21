3290e11783b7d53e0fb5d47647d8578e46654310
Adrian Mora <adriandariom@gmail.com>
2015-06-21 16:24:27 -0400

    Added more basic project structure. Set the chromedriver path.

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -3,6 +3,7 @@ package Core;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
 import java.util.Arrays;
 
 /**
@@ -10,22 +11,40 @@ import java.util.Arrays;
  */
 public class Main {
 
-    public static final String[] VALID_FLAGS = {"--full", "--front", "--config"};
+    public static final String[] VALID_FLAGS = {"--full", "--front", "--config"}; // make set?
 
     public static void main(String[] args) {
 
-        System.out.println("Made it here");
+        System.out.println("Made it here"); // marker; delete later
 
-        String[] urlAndFlags = Utils.parseArgs(args);
-        String url = urlAndFlags[0];
-        String[] flags = Arrays.copyOfRange(urlAndFlags, 1, urlAndFlags.length);
+        File chromedriver = new File("/Users/AdrianM/Google Drive/CodingProjects/JavaProjects/goto/chromedriver");
+        System.setProperty("webdriver.chrome.driver", chromedriver.getAbsolutePath());
 
-        if (Utils.validateArgs(url, flags)) {
-            WebDriver driver = new ChromeDriver();
+        String url;
+        String[] flags;
+        String invalidArgMessage = "Invalid argument(s). Use the '--help' flag for help.";
 
+
+        if (args.length > 0) {
+            url = args[0];
+            flags = Arrays.copyOfRange(args, 1, args.length);
+
+            if (Utils.validateArgs(url, flags)) {
+                WebDriver driver = new ChromeDriver();
+                url = Utils.formatURL(url);
+                driver.get(url); // find out what does and does not cause an error here
+
+                for (String flag : flags) {
+                }
+
+            } else {
+                System.out.println(invalidArgMessage);
+            }
         } else {
-            System.out.println(invalidArgMesage);
+            System.out.println(invalidArgMessage);
         }
+
+
 
 
     }
--- src/Core/Utils.java
+++ src/Core/Utils.java
@@ -5,5 +5,11 @@ package Core;
  */
 public class Utils {
 
+    public static boolean validateArgs(String url, String[] flags) {
 
+    }
+
+    public static String formatURL(String url) {
+
+    }
 }
